d43a2ce427219585d0f778f9b1ec69801d1c9362
Mohamed Salad <msalad@uncc.edu>
2016-10-20 16:36:34 -0400

    Added Sample fragments
    -To be added to Tabs

--- app/src/main/java/com/example/itsadmin/viewpager/HomeActivity.java
+++ app/src/main/java/com/example/itsadmin/viewpager/HomeActivity.java
@@ -1,10 +1,10 @@
 package com.example.itsadmin.viewpager;
 
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class HomeActivity extends AppCompatActivity {
-
+public class HomeActivity extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
---
+++ app/src/main/java/com/example/itsadmin/viewpager/SampleFragment2.java
@@ -0,0 +1,10 @@
+package com.example.itsadmin.viewpager;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by itsadmin on 10/20/2016.
+ */
+
+public class SampleFragment2 extends Fragment {
+}
