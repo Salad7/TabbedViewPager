7bafe0a8c5f8bee230430829e300e4fac53c3cc5
Mohamed Salad <msalad@uncc.edu>
2016-10-21 10:53:27 -0400

    Added ActionBar \ PageViewer listeners
    details to show tabs in action bar
    respond to swipes across the phone

--- app/src/main/java/com/example/itsadmin/viewpager/HomeActivity.java
+++ app/src/main/java/com/example/itsadmin/viewpager/HomeActivity.java
@@ -1,5 +1,7 @@
 package com.example.itsadmin.viewpager;
 
+import android.app.ActionBar;
+import android.app.FragmentTransaction;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
@@ -17,9 +19,62 @@ public class HomeActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        final ActionBar actionBar = getActionBar();
+
+        //Specify that tabs should be displayed in the action bar
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
         PagerAdapter pa = new PagerAdapter(getSupportFragmentManager());
         viewPager = (ViewPager) findViewById(R.id.pager);
+        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                 //When swiping between pages,
+                //Select the corresponding tab.
+                getActionBar().setNavigationMode(position);
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
         viewPager.setAdapter(pa);
+
+        //Create a tab listener that is called when the user changes tabs
+        ActionBar.TabListener tabListener = new ActionBar.TabListener() {
+            @Override
+            public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+                //Show the given tab
+
+                //When the tab is selected, switch
+                //To the corresponding page in the viewpager
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+                //Hide the given tab
+            }
+
+            @Override
+            public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+                //ignore for now
+            }
+        };
+
+        for(int i = 0; i < 2; i++){
+            actionBar.addTab(actionBar.newTab().setText("Tab " + (i+1))
+            .setTabListener(tabListener));
+        }
+
+
 
 
     }
